e017f50e59b613114d81e790a0057b29d3c9352e
lydiatar <saducolindy@gmail.com>
2026-08-31 00:56:08 +0800

    Update local_storage_manager.js

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -6,7 +6,9 @@ window.fakeStorage = {
   },
 
   getItem: function (id) {
-    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+    return this._data.hasOwnProperty(id)
+      ? this._data[id]
+      : undefined;
   },
 
   removeItem: function (id) {
@@ -14,16 +16,19 @@ window.fakeStorage = {
   },
 
   clear: function () {
-    return this._data = {};
+    this._data = {};
   }
 };
 
 function LocalStorageManager() {
-  this.bestScoreKey     = "bestScore";
-  this.gameStateKey     = "gameState";
+  this.bestScoreKey = "bestScore";
+  this.gameStateKey = "gameState";
 
   var supported = this.localStorageSupported();
-  this.storage = supported ? window.localStorage : window.fakeStorage;
+
+  this.storage = supported
+    ? window.localStorage
+    : window.fakeStorage;
 }
 
 LocalStorageManager.prototype.localStorageSupported = function () {
@@ -31,16 +36,23 @@ LocalStorageManager.prototype.localStorageSupported = function () {
 
   try {
     var storage = window.localStorage;
+
     storage.setItem(testKey, "1");
     storage.removeItem(testKey);
+
     return true;
   } catch (error) {
     return false;
   }
 };
 
-// Best score getters/setters
+
+// -------------------------
+// BEST SCORE
+// -------------------------
+
 LocalStorageManager.prototype.getBestScore = function () {
+  // Multiplayer always starts with a fresh best score.
   if (window.multiplayerMode) {
     return 0;
   }
@@ -48,7 +60,9 @@ LocalStorageManager.prototype.getBestScore = function () {
   return this.storage.getItem(this.bestScoreKey) || 0;
 };
 
+
 LocalStorageManager.prototype.setBestScore = function (score) {
+  // Don't save multiplayer scores as permanent best scores.
   if (window.multiplayerMode) {
     return;
   }
@@ -56,18 +70,38 @@ LocalStorageManager.prototype.setBestScore = function (score) {
   this.storage.setItem(this.bestScoreKey, score);
 };
 
-// Game state getters/setters and clearing
-f (window.multiplayerMode) {
+
+// -------------------------
+// GAME STATE
+// -------------------------
+
+LocalStorageManager.prototype.getGameState = function () {
+  // Multiplayer games always start fresh.
+  if (window.multiplayerMode) {
     return null;
   }
 
-  var stateJSON = this.storage.getItem(this.gameStateKey);
-  return stateJSON ? JSON.parse(stateJSON) : null;
+  var stateJSON =
+    this.storage.getItem(this.gameStateKey);
+
+  return stateJSON
+    ? JSON.parse(stateJSON)
+    : null;
 };
 
+
 LocalStorageManager.prototype.setGameState = function (gameState) {
-  this.storage.setItem(this.gameStateKey, JSON.stringify(gameState));
+  // Multiplayer state does not need to persist across page reloads.
+  if (window.multiplayerMode) {
+    return;
+  }
+
+  this.storage.setItem(
+    this.gameStateKey,
+    JSON.stringify(gameState)
+  );
 };
+
 
 LocalStorageManager.prototype.clearGameState = function () {
   this.storage.removeItem(this.gameStateKey);
