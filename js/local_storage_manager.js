c796735e9301e76caff19345992a19f3a3f6f65c
lydiatar <saducolindy@gmail.com>
2026-08-31 03:19:22 +0800

    Update local_storage_manager.js

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -1,197 +1,198 @@
 window.fakeStorage = {
   _data: {},
 
-  setItem: function (
-    id,
-    val
-  ) {
-
-    return (
-      this._data[id] =
-        String(val)
-    );
+  setItem: function (id, val) {
+    return (this._data[id] = String(val));
   },
 
-
-  getItem: function (
-    id
-  ) {
-
-    return (
-      this._data.hasOwnProperty(id)
-        ? this._data[id]
-        : undefined
-    );
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id)
+      ? this._data[id]
+      : undefined;
   },
 
-
-  removeItem: function (
-    id
-  ) {
-
-    return delete
-      this._data[id];
+  removeItem: function (id) {
+    return delete this._data[id];
   },
-
 
   clear: function () {
-
-    this._data =
-      {};
+    this._data = {};
   }
 };
 
-
 function LocalStorageManager() {
+  this.bestScoreKey = "rinas2048.solo.bestScore";
+  this.gameStateKey = "rinas2048.solo.gameState";
+  this.undoStackKey = "rinas2048.solo.undoStack";
+  this.highestTileEverKey = "rinas2048.solo.highestTileEver";
 
-  this.bestScoreKey =
-    "bestScore";
+  this.legacyBestScoreKey = "bestScore";
+  this.legacyGameStateKey = "gameState";
 
+  var supported = this.localStorageSupported();
 
-  this.gameStateKey =
-    "gameState";
-
-
-  var supported =
-    this.localStorageSupported();
-
-
-  this.storage =
-    supported
-
-      ? window.localStorage
-
-      : window.fakeStorage;
+  this.storage = supported
+    ? window.localStorage
+    : window.fakeStorage;
 }
 
+LocalStorageManager.prototype.localStorageSupported = function () {
+  var testKey = "test";
 
-LocalStorageManager.prototype.localStorageSupported =
-  function () {
+  try {
+    var storage = window.localStorage;
+    storage.setItem(testKey, "1");
+    storage.removeItem(testKey);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
 
-    var testKey =
-      "test";
+LocalStorageManager.prototype.isMultiplayer = function () {
+  return !!window.multiplayerMode;
+};
 
+LocalStorageManager.prototype.getBestScore = function () {
+  if (this.isMultiplayer()) {
+    return 0;
+  }
 
-    try {
+  var saved = Number(this.storage.getItem(this.bestScoreKey) || 0);
+  var legacy = Number(this.storage.getItem(this.legacyBestScoreKey) || 0);
 
-      var storage =
-        window.localStorage;
+  return Math.max(saved, legacy);
+};
 
+LocalStorageManager.prototype.setBestScore = function (score) {
+  if (this.isMultiplayer()) {
+    return;
+  }
 
-      storage.setItem(
-        testKey,
-        "1"
-      );
+  this.storage.setItem(this.bestScoreKey, score);
+};
 
+LocalStorageManager.prototype.getGameState = function () {
+  if (this.isMultiplayer()) {
+    return null;
+  }
 
-      storage.removeItem(
-        testKey
-      );
+  var stateJSON = this.storage.getItem(this.gameStateKey);
 
+  if (!stateJSON) {
+    stateJSON = this.storage.getItem(this.legacyGameStateKey);
+  }
 
-      return true;
+  if (!stateJSON) {
+    return null;
+  }
 
-    } catch (
-      error
-    ) {
+  try {
+    return JSON.parse(stateJSON);
+  } catch (error) {
+    return null;
+  }
+};
 
-      return false;
-    }
-  };
+LocalStorageManager.prototype.hasGameState = function () {
+  return !!this.getGameState();
+};
 
+LocalStorageManager.prototype.setGameState = function (gameState) {
+  if (this.isMultiplayer()) {
+    return;
+  }
 
-LocalStorageManager.prototype.getBestScore =
-  function () {
+  this.storage.setItem(
+    this.gameStateKey,
+    JSON.stringify(gameState)
+  );
+};
 
-    if (
-      window.multiplayerMode
-    ) {
+LocalStorageManager.prototype.clearGameState = function () {
+  if (this.isMultiplayer()) {
+    return;
+  }
 
-      return 0;
-    }
+  this.storage.removeItem(this.gameStateKey);
+  this.storage.removeItem(this.legacyGameStateKey);
+};
 
+LocalStorageManager.prototype.getUndoStack = function () {
+  if (this.isMultiplayer()) {
+    return [];
+  }
 
-    return (
-      this.storage.getItem(
-        this.bestScoreKey
-      ) || 0
-    );
-  };
+  var stackJSON = this.storage.getItem(this.undoStackKey);
 
+  if (!stackJSON) {
+    return [];
+  }
 
-LocalStorageManager.prototype.setBestScore =
-  function (
-    score
-  ) {
+  try {
+    var stack = JSON.parse(stackJSON);
+    return Array.isArray(stack) ? stack : [];
+  } catch (error) {
+    return [];
+  }
+};
 
-    if (
-      window.multiplayerMode
-    ) {
-      return;
-    }
+LocalStorageManager.prototype.setUndoStack = function (stack) {
+  if (this.isMultiplayer()) {
+    return;
+  }
 
+  var safeStack = Array.isArray(stack) ? stack.slice() : [];
 
+  try {
     this.storage.setItem(
-      this.bestScoreKey,
-      score
+      this.undoStackKey,
+      JSON.stringify(safeStack)
     );
-  };
+  } catch (error) {
+    // If browser storage fills up, trim the oldest history and retry.
+    while (safeStack.length > 50) {
+      safeStack.splice(0, 50);
 
-
-LocalStorageManager.prototype.getGameState =
-  function () {
-
-    if (
-      window.multiplayerMode
-    ) {
-
-      return null;
+      try {
+        this.storage.setItem(
+          this.undoStackKey,
+          JSON.stringify(safeStack)
+        );
+        return;
+      } catch (retryError) {
+        // Keep trimming until it fits.
+      }
     }
+  }
+};
 
+LocalStorageManager.prototype.clearUndoStack = function () {
+  if (this.isMultiplayer()) {
+    return;
+  }
 
-    var stateJSON =
-      this.storage.getItem(
-        this.gameStateKey
-      );
+  this.storage.removeItem(this.undoStackKey);
+};
 
+LocalStorageManager.prototype.getHighestTileEver = function () {
+  if (this.isMultiplayer()) {
+    return 0;
+  }
 
-    return stateJSON
+  return Number(
+    this.storage.getItem(this.highestTileEverKey) || 0
+  );
+};
 
-      ? JSON.parse(
-          stateJSON
-        )
+LocalStorageManager.prototype.setHighestTileEver = function (value) {
+  if (this.isMultiplayer()) {
+    return;
+  }
 
-      : null;
-  };
+  var current = this.getHighestTileEver();
 
-
-LocalStorageManager.prototype.setGameState =
-  function (
-    gameState
-  ) {
-
-    if (
-      window.multiplayerMode
-    ) {
-      return;
-    }
-
-
-    this.storage.setItem(
-
-      this.gameStateKey,
-
-      JSON.stringify(
-        gameState
-      )
-    );
-  };
-
-
-LocalStorageManager.prototype.clearGameState =
-  function () {
-
-    this.storage.removeItem(
-      this.gameStateKey
-    );
-  };
+  if (value > current) {
+    this.storage.setItem(this.highestTileEverKey, value);
+  }
+};
