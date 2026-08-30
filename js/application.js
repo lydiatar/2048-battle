dd0f9ac67be24e3523a31e561423609f4adff153
lydiatar <saducolindy@gmail.com>
2026-08-31 00:42:05 +0800

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,4 +1,9 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  window.multiplayerGame = new GameManager(
+    4,
+    KeyboardInputManager,
+    HTMLActuator,
+    LocalStorageManager
+  );
 });
