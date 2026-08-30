b2b5812ea7caf60f8205f5b16ac0a54fc47b56f1
lydiatar <saducolindy@gmail.com>
2026-08-31 03:19:37 +0800

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,13 +1,20 @@
-// Wait till the browser is ready to render the game.
-window.requestAnimationFrame(
-  function () {
+window.currentGameMode = "menu";
+window.multiplayerMatchActive = false;
+window.multiplayerGameOver = false;
+window.multiplayerAllowRestart = false;
+window.multiplayerSecondChanceUsed = false;
+window.multiplayerTargetTile = 2048;
 
-    window.multiplayerGame =
-      new GameManager(
-        4,
-        KeyboardInputManager,
-        HTMLActuator,
-        LocalStorageManager
-      );
-  }
-);
+// Boot the hidden game engine without reading or overwriting Solo saves.
+window.multiplayerMode = true;
+
+window.requestAnimationFrame(function () {
+  window.multiplayerGame = new GameManager(
+    4,
+    KeyboardInputManager,
+    HTMLActuator,
+    LocalStorageManager
+  );
+
+  window.multiplayerMode = false;
+});
